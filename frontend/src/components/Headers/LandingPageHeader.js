0f99a6c1a018a2865a73bf889f8d6c291b3e4563
Prabin Lamichhane <nibarplc@gmail.com>
2020-01-25 23:50:28 +0545

    hire now button works

--- frontend/src/components/Headers/LandingPageHeader.js
+++ frontend/src/components/Headers/LandingPageHeader.js
@@ -1,53 +1,67 @@
-
 import React from "react";
 
 // reactstrap components
-import { Button, Container,  InputGroup, Input, InputGroupAddon, InputGroupText} from "reactstrap";
+import {
+	Button,
+	Container,
+	InputGroup,
+	Input,
+	InputGroupAddon,
+	InputGroupText
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 // core components
 
 function LandingPageHeader() {
-  let pageHeader = React.createRef();
+	let pageHeader = React.createRef();
 
-  React.useEffect(() => {
-    if (window.innerWidth < 991) {
-      const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 3;
-        pageHeader.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
-      return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
-    }
-  });
+	React.useEffect(() => {
+		if (window.innerWidth < 991) {
+			const updateScroll = () => {
+				let windowScrollTop = window.pageYOffset / 3;
+				pageHeader.current.style.transform =
+					"translate3d(0," + windowScrollTop + "px,0)";
+			};
+			window.addEventListener("scroll", updateScroll);
+			return function cleanup() {
+				window.removeEventListener("scroll", updateScroll);
+			};
+		}
+	});
 
-  return (
-    <>
-      <div
-        style={{
-          backgroundImage: "url(" + require("assets/img/village.jpg") + ")"
-        }}
-        className="page-header"
-        data-parallax={true}
-        ref={pageHeader}
-      >
-        <div className="filter" />
-        <Container>
-          <div className="motto text-center">
-            <h1>SAHAYATRI</h1>
-            <h3>Creating Interface Between Locals and Tourists</h3>
-            <br />
-            <InputGroup className='w-75 mx-auto'>
-                <Input placeholder="Search" type="search" className='bg-transparent  text-white' />
-                 <InputGroupAddon addonType="apppend">
-                  <InputGroupText className='bg-transparent'>
-                    <i className="fa fa-search text-white view overlay zoom" onClick={()=>console.log('this')}/>
-                  </InputGroupText>
-                </InputGroupAddon>
-            </InputGroup>
-            {/* <Button
+	return (
+		<>
+			<div
+				style={{
+					backgroundImage:
+						"url(" + require("assets/img/village.jpg") + ")"
+				}}
+				className="page-header"
+				data-parallax={true}
+				ref={pageHeader}
+			>
+				<div className="filter" />
+				<Container>
+					<div className="motto text-center">
+						<h1>SAHAYATRI</h1>
+						<h3>Creating Interface Between Locals and Tourists</h3>
+						<br />
+						<Link to="/filter">
+							<InputGroup className="w-75 mx-auto">
+								<Input
+									placeholder="Search"
+									type="search"
+									className="bg-transparent  text-white"
+								/>
+								<InputGroupAddon addonType="apppend">
+									<InputGroupText className="bg-transparent">
+										<i className="fa fa-search text-white view overlay zoom" />
+									</InputGroupText>
+								</InputGroupAddon>
+							</InputGroup>
+						</Link>
+						{/* <Button
               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
               className="btn-round mr-1"
               color="neutral"
@@ -60,11 +74,11 @@ function LandingPageHeader() {
             <Button className="btn-round" color="neutral" type="button" outline>
               Download
             </Button> */}
-          </div>
-        </Container>
-      </div>
-    </>
-  );
+					</div>
+				</Container>
+			</div>
+		</>
+	);
 }
 
 export default LandingPageHeader;
--- frontend/src/views/examples/ProfilePage.js
+++ frontend/src/views/examples/ProfilePage.js
@@ -1,147 +1,162 @@
-
 import React from "react";
 
-import url from 'url.js'
+import url from "url.js";
 import Gallery from "react-photo-gallery";
 
 // reactstrap components
 import {
-  Button,
-  Label,
-  FormGroup,
-  Input,
-  NavItem,
-  NavLink,
-  Nav,
-  TabContent,
-  TabPane,
-  Container,
-  Row,
-  Col,
-  UncontrolledCarousel
+	Button,
+	Label,
+	FormGroup,
+	Input,
+	NavItem,
+	NavLink,
+	Nav,
+	TabContent,
+	TabPane,
+	Container,
+	Row,
+	Col,
+	UncontrolledCarousel
 } from "reactstrap";
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
-import axios from 'axios'
+import axios from "axios";
 
 function ProfilePage(props) {
-  const [user, setUser] = React.useState({});
+	const [user, setUser] = React.useState({});
 
+	const [activeTab, setActiveTab] = React.useState("1");
 
-  const [activeTab, setActiveTab] = React.useState("1");
+	const toggle = tab => {
+		if (activeTab !== tab) {
+			setActiveTab(tab);
+		}
+	};
 
+	document.documentElement.classList.remove("nav-open");
+	var items = [];
+	React.useEffect(() => {
+		const id = props.match.params.id;
+		axios.get(url + "/api/sathi/" + id).then(e => {
+			setUser(e.data[0], () => {
+				if (user !== {}) {
+					var image = user.image.slice(0, 3);
+					var items = image.map(img => {
+						return {
+							src: url + img,
+							width: 4,
+							height: 3
+						};
+					});
+				}
+			});
+		});
+		document.body.classList.add("/");
+		return function cleanup() {
+			document.body.classList.remove("/");
+		};
+	}, []);
+	console.log(user);
 
-  const toggle = tab => {
-    if (activeTab !== tab) {
-      setActiveTab(tab);
-    }
-  };
+	const hireNow = e => {
+		e.preventDefault();
+		props.history.push("/checkout");
+	};
 
-  document.documentElement.classList.remove("nav-open");
-var items=[]
-  React.useEffect(() => {
-    const id = props.match.params.id
-    axios.get(url + '/api/sathi/' + id).then(e => {
-      setUser(e.data[0],()=>{
-        if (user !== {}) {
-          var image = user.image.slice(0,3)
-          var items = image.map(img => {
-            return {
-              src: url + img,
-              width: 4,
-              height: 3
-            }
-          })
-        }
-      })
-     
-    })
-    document.body.classList.add("/");
-    return function cleanup() {
-      document.body.classList.remove("/");
-    };
-  }, []);
-  console.log(user)
-
-
-  return (
-    <div>
-      <ExamplesNavbar />
-      <ProfilePageHeader />
-      <div className="section profile-content">
-        <Container>
-          <div className="owner">
-            <div className="avatar">
-              <img
-                alt="..."
-                className="img-circle img-no-padding img-responsive"
-              // src={url + user.image[0]}
-              />
-            </div>
-            <div className="name">
-              <h2 className="title text-dark">
-                {user.name} <br />
-              </h2>
-            </div>
-          </div>
-          <Row>
-            <Col className="ml-auto mr-auto text-center text-dark" md="6">
-              <h6 className="description text-dark">Description</h6>
-              <p>
-                {user.description}
-              </p>
-              <br />
-              {/* <Button className="btn-round" color="default" outline>
+	return (
+		<div>
+			<ExamplesNavbar />
+			<ProfilePageHeader />
+			<div className="section profile-content">
+				<Container>
+					<div className="owner">
+						<div className="avatar">
+							<img
+								alt="..."
+								className="img-circle img-no-padding img-responsive"
+								// src={url + user.image[0]}
+							/>
+						</div>
+						<div className="name">
+							<h2 className="title text-dark">
+								{user.name} <br />
+							</h2>
+						</div>
+					</div>
+					<Row>
+						<Col
+							className="ml-auto mr-auto text-center text-dark"
+							md="6"
+						>
+							<h6 className="description text-dark">
+								Description
+							</h6>
+							<p>{user.description}</p>
+							<br />
+							{/* <Button className="btn-round" color="default" outline>
                       <i className="fa fa-cog" /> Settings
           </Button> */}
-            </Col>
-          </Row>
-          <Row>
-            <Col className="ml-auto mr-auto text-center text-dark" md="6">
-              <h6 className="title text-dark">Languages
-                <br />
-                <p>{user.languages}</p>
-              </h6>
+						</Col>
+					</Row>
+					<Row>
+						<Col
+							className="ml-auto mr-auto text-center text-dark"
+							md="6"
+						>
+							<h6 className="title text-dark">
+								Languages
+								<br />
+								<p>{user.languages}</p>
+							</h6>
 
-              <h6 className="title text-dark">Interests
-                <br />
-                <p>{user.interests}</p>
-              </h6>
+							<h6 className="title text-dark">
+								Interests
+								<br />
+								<p>{user.interests}</p>
+							</h6>
 
-              <h6 className="title text-dark">Location
-                <br />
-                <p>{user.places}</p>
-              </h6>
-              <h4><strong>Photos</strong></h4>
-              <Gallery photos={items}/> 
-
-            </Col>
-          </Row>
-          <br />
-          <Col className="ml-auto mr-auto text-center text-dark" md="6">
-            <Button className="btn-round w-25 h6 font-weight-bold" color="primary">
-              Hire Now
-              </Button>
-          </Col>
-          <DemoFooter />
-        </Container>
-
-      </div>
-
-    </div>
-  )
-  // }
-  // else{
-  //   return (
-  //     <div>
-  //       <ExamplesNavbar />
-  //         <ProfilePageHeader />
-  //    <Gallery photos={items} />
-  //    </div>
-  //   )
-  // }
+							<h6 className="title text-dark">
+								Location
+								<br />
+								<p>{user.places}</p>
+							</h6>
+							<h4>
+								<strong>Photos</strong>
+							</h4>
+							<Gallery photos={items} />
+						</Col>
+					</Row>
+					<br />
+					<Col
+						className="ml-auto mr-auto text-center text-dark"
+						md="6"
+					>
+						<Button
+							className="btn-round w-25 h6 font-weight-bold"
+							color="primary"
+							onClick={hireNow}
+						>
+							Hire Now
+						</Button>
+					</Col>
+					<DemoFooter />
+				</Container>
+			</div>
+		</div>
+	);
+	// }
+	// else{
+	//   return (
+	//     <div>
+	//       <ExamplesNavbar />
+	//         <ProfilePageHeader />
+	//    <Gallery photos={items} />
+	//    </div>
+	//   )
+	// }
 }
 export default ProfilePage;
